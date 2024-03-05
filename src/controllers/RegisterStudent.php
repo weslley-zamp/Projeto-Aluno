cd48eed2624ef842c3aa59a971cb853fa7c5c98a
Braio <mernickbryan@gmail.com>
2024-03-05 20:18:26 -0300

    feat: ajuste

--- src/controllers/RegisterStudent.php
+++ src/controllers/RegisterStudent.php
@@ -1,48 +1,55 @@
 <?php
+require_once('../models/student.php');
+
 class RegisterStudent
 {
     private $students = [];
 
     public function __construct()
     {
-        $this->students[] = new Student(1, 'Braio', '2024031', 'Moda');
-        $this->students[] = new Student(2, 'Zampier', '2024032', 'Engenheiro');
+        $this->students[] = new Student(1, 'Braio', '2024031', 'Moda', '1234');
+        $this->students[] = new Student(2, 'Zampier', '2024032', 'Engenheiro', '1234');
     }
 
-    public function addStudent(Student $student){
-        try{
-            foreach($this->students as $user){
-                if( $user->registration == $student->registration ){
-                    return throw Exception;
+    public function addStudent(Student $student)
+    {
+        try {
+            foreach ($this->students as $user) {
+                if ($user->registration == $student->registration) {
+                    throw new Exception('Estudante já cadastrado com essa matrícula.');
                 }
             }
             $this->students[] = $student;
-        } catch(Exception $e) {
+        } catch (Exception $e) {
             return $e;
         }
     }
 
-    public function getStudents(){
+    public function getStudents()
+    {
         return $this->students;
     }
 
-    public function updateStudent(Student $student){
-        foreach($this->students as $user){
-            if( $user->id == $student->id ){
-                return $this->students[$user->id] = $student;
+    public function updateStudent(Student $student)
+    {
+        foreach ($this->students as $key => $user) {
+            if ($user->id == $student->id) {
+                $this->students[$key] = $student;
+                return true;
             }
         }
+        return false;
     }
 
-    public function removeStudent(Student $student){
-        foreach($this->students as $user){
-            if( $user->id == $student->id ){
-                //remove from the list
-                return $this->students[] = $student;
+    public function removeStudent(Student $student)
+    {
+        foreach ($this->students as $key => $user) {
+            if ($user->id == $student->id) {
+                unset($this->students[$key]);
+                return true;
             }
         }
+        return false;
     }
-
-
 }
 ?>
